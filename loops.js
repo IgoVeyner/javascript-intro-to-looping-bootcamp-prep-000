d80c39f440376e64ae1ba00385a8fe85e9c95b4f
Igor Veyner <igorveyner95@gmail.com>
2020-02-25 11:09:42 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,38 @@
+/*function forLoop(array){
+  for(var i = 0; i < 35; i++){
+    array[i]= "I am " + i + " strange loops.";
+    if(i === 11){
+      array[i] = "I am 1 strange loop.";
+    }
+    if(i === 34){
+      array[i] = "I am 24 strange loops."
+    }
+  }
+  return array;
+}
+*/
+// use array.push
+
+function whileLoop(n) {
+  while(n>0){
+    console.log(n);
+    n--;
+  }
+  return 'done';
+}
+
+/*function doWhileLoop(num){
+  do{
+    console.log("I run once regardless")
+  } while 
+  (incrementVariable() < num);
+}
+
+function incrementVariable() {
+  var i = i + 1;
+  return i;
+}
+
+var i = 0;
+var num = 10;
+*/
