c5f92aba4ec7ebc87d0e0ee41f3dd0208c57399a
Igor Veyner <igorveyner95@gmail.com>
2020-02-25 11:12:32 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -14,9 +14,9 @@
 // use array.push
 function forLoop(array){
   for(var i = 0; i < 25; i++){
-    array.push("I am " + i + " strange loops.";
+    array.push("I am " + i + " strange loops.");
     if(i === 1){
-      array.push = "I am 1 strange loop.";
+      array.push("I am 1 strange loop.");
     }
   }
   return array;
